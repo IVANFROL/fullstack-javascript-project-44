812182356853cc6b537355b3f246b07857086ea1
IVANFROL <frolovitch.ivan@mail.ru>
2024-11-08 20:28:27 +0300

    Игра: Калькулятор

---
+++ src/GameMechanics/calc.js
@@ -0,0 +1,39 @@
+import randomNumber from "../utils.js";
+import startGames from "../index.js";
+
+const resultExpression = (firstValue, operator, secondValue) => {
+  switch (operator) {
+    case "+":
+      return firstValue + secondValue;
+    case "-":
+      return firstValue - secondValue;
+    case "*":
+      return firstValue * secondValue;
+    default:
+      throw new Error(`Unknown operator: '${operator}'!`);
+  }
+};
+
+const operators = ["+", "-", "*"];
+
+const gamesDescrip = "What is the result of the expression?";
+
+const generateRoundsData = () => {
+  const firstNumber = randomNumber(1, 25);
+  const secondNumber = randomNumber(1, 25);
+  const operator = operators[randomNumber(0, operators.length - 1)];
+
+  const question = `${firstNumber} ${operator} ${secondNumber}`;
+  const resultOfExpression = resultExpression(
+    firstNumber,
+    operator,
+    secondNumber
+  );
+  const correctAnswer = resultOfExpression.toString();
+
+  return [question, correctAnswer];
+};
+
+export default () => {
+  startGames(generateRoundsData, gamesDescrip);
+};
