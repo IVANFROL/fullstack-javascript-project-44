57138f3033e2859e2580e8be463c61f297e745c1
IVANFROL <frolovitch.ivan@mail.ru>
2024-12-20 14:09:06 +0300

    fix project

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
+
+const roundsCount = 3; // Системная настройка: количество раундов
 
 export default (generateRounds, gamesDescrip) => {
-  console.log('Welcome to the Brain Games!');
-  const nameUser = readlineSync.question('May I have your name? ');
+  console.log("Welcome to the Brain Games!");
+
+  const nameUser = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${nameUser}!`);
 
   console.log(gamesDescrip);
 
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < roundsCount; i += 1) {
     const random = generateRounds();
     const [question, correctAnswer] = random;
+
     console.log(`Question: ${question}`);
-    const youAnswer = readlineSync.question('Your answer: ');
+
+    const youAnswer = readlineSync.question("Your answer: ");
 
     if (youAnswer !== correctAnswer) {
       console.log(
-        `${youAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`,
+        `${youAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`
       );
       console.log(`Let's try again, ${nameUser}!`);
       return;
     }
-    console.log('Correct!');
+
+    console.log("Correct!");
   }
+
   console.log(`Congratulations, ${nameUser}!`);
 };
